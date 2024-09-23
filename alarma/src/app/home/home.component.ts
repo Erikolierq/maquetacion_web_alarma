e7c2b150563738d391cf3a41824875128de5d518
Erikolierq <e.olierq@uniandes.edu.co>
2024-09-23 14:24:45 -0500

    add home component

---
+++ alarma/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';  
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FontAwesomeModule,CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
