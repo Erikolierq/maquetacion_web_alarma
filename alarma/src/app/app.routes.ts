873e4aedd75556695465542032ce27f2c36b3708
Erikolierq <e.olierq@uniandes.edu.co>
2024-09-23 22:30:06 -0500

    inicio pantalla 3

--- alarma/src/app/app.routes.ts
+++ alarma/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeLoginComponent } from './home-login/home-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },  // Ruta para la página de inicio
+    { path: 'home', component: HomeLoginComponent },  // Ruta para el componente home-login
+    { path: '**', redirectTo: '' }  // Redirigir a home por defecto
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- alarma/src/app/home/home.component.ts
+++ alarma/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';  
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FontAwesomeModule,CommonModule],
+  imports: [FontAwesomeModule,CommonModule,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  
+
+  
+
   
 }
